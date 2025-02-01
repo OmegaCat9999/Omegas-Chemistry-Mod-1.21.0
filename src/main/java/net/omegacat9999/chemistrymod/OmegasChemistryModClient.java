9569af56700fbf1e0a81e9aea318f0ce81119a8c
OmegaCat9999 <omegacat9999@gmail.com>
2025-02-01 22:25:11 +0800

    Initial Commit

---
+++ src/main/java/net/omegacat9999/chemistrymod/OmegasChemistryModClient.java
@@ -0,0 +1,10 @@
+package net.omegacat9999.chemistrymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OmegasChemistryModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/omegacat9999/chemistrymod/OmegasChemistryModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.omegacat9999.chemistrymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OmegasChemistryModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
