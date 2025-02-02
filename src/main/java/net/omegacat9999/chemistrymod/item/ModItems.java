0724fc4ec2fbc2596ce740f5f8cdfbc07bbc396c
OmegaCat9999 <omegacat9999@gmail.com>
2025-02-02 20:37:21 +0800

    New ModItems class.

---
+++ src/main/java/net/omegacat9999/chemistrymod/item/ModItems.java
@@ -0,0 +1,46 @@
+package net.omegacat9999.chemistrymod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.omegacat9999.chemistrymod.OmegasChemistryMod;
+
+public class ModItems {
+
+    // Define Items (please define in alphabetical order)
+    // Element_ELEMENT Items
+    public static final Item CARBON_ELEMENT = registerItem("carbon_element", new Item(new Item.Settings()));
+
+    // Element Items
+
+
+    // Compound Items
+
+    // Add Item(s) to Vanilla Item Group (unused, may remove.)
+    /*
+    private static void AddItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+
+    }
+    */
+
+    // Register Item(s)
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(OmegasChemistryMod.MOD_ID, name), item);
+    }
+
+    // Register All Mod Items
+    public static void registerModItems() {
+        // Log Item Register Indicator (Debugging)
+        net.omegacat9999.chemistrymod.OmegasChemistryMod.LOGGER
+                .info("Registering Mod Items for" + net.omegacat9999.chemistrymod.OmegasChemistryMod.MOD_ID);
+
+        // Call registerItem Method for INGREDIENTS Vanilla Item Group (directly)
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register(entries -> {
+            entries.add(CARBON_ELEMENT);
+        });
+    }
+}
