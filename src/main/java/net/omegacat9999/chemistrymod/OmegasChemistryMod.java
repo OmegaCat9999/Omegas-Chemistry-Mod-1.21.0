c8c6fef65db8a805278cd989e3200ccf588a0e75
OmegaCat9999 <omegacat9999@gmail.com>
2025-02-02 14:48:58 +0800

    Changed MOD_ID from "omega-chemistry-mod" to "omegachemistry"

--- src/main/java/net/omegacat9999/chemistrymod/OmegasChemistryMod.java
+++ src/main/java/net/omegacat9999/chemistrymod/OmegasChemistryMod.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OmegasChemistryMod implements ModInitializer {
-	public static final String MOD_ID = "omega-chemistry-mod";
+	public static final String MOD_ID = "omegachemistry";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
